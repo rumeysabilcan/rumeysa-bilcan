b0b9cd2f1c40aea26f155722a23ba234b1395a22
rumeysabilcan <rumeysablcn@gmail.com>
2024-06-24 17:52:05 +0300

    Add files via upload

---
+++ öklid.py
@@ -0,0 +1,12 @@
+import math
+points = [(1, 2), (4, 6), (7, 8), (2, 1)]
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+min_distance = min(distances)
+print(f"Noktalar arasındaki minimum mesafe: {min_distance}")
